3a72a333dafc5e2d1aada0707504f83c7db0b54c
doan201203 <doan201203@gmail.com>
2024-10-06 21:31:08 +0700

    add: new page face verification

--- pages/Face Verification.py
+++ pages/Face Verification.py
@@ -4,7 +4,8 @@ from google.cloud import firestore
 st.set_page_config(page_title="Face Verification", initial_sidebar_state="expanded")
 st.title("Face Verification")
 
-db = firestore.Client.from_service_account_json('demo2-2a1d9-firebase-adminsdk-yd2h4-1f3ca9dd12.json')
+db = firestore.Client.from_service_account_info(st.secrets)
+# db = firestore.Client.from_service_account_json('demo2-2a1d9-firebase-adminsdk-yd2h4-1f3ca9dd12.json')
 doc_ref = db.collection('face_dataset').document('Face1')
 
 doc = doc_ref.get()
