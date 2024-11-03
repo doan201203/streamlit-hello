a4be2f177bd07abe28a9b6661aba3c2570b79633
doan201203 <doan201203@gmail.com>
2024-11-04 03:16:33 +0700

    DONE: keypoint matching

--- my_utils/cbri.py
+++ my_utils/cbri.py
@@ -1,8 +1,8 @@
-from superpoint import SuperPointFrontend
+# from superpoint import SuperPointFrontend
 
-class CBRI:
-    def __init__(self):
-        self.detector = SuperPointFrontend(weights_path='./my_utils/superpoint_v1.pth', nms_dist=4, conf_thresh=0.015, nn_thresh=0.7)
-        self.
-    def embed(self):
-      pass
+# class CBRI:
+#     def __init__(self):
+#         self.detector = SuperPointFrontend(weights_path='./my_utils/superpoint_v1.pth', nms_dist=4, conf_thresh=0.015, nn_thresh=0.7)
+#         self.
+#     def embed(self):
+#       pass
