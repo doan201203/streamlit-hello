d6ce871d193a392562b6e6f879fcbc2031579592
doan201203 <doan201203@gmail.com>
2024-11-04 04:20:56 +0700

    DONE: keypoint matching

--- my_utils/cbri.py
+++ my_utils/cbri.py
@@ -1,8 +1,34 @@
-# from superpoint import SuperPointFrontend
+from my_utils.superpoint import SuperPointFrontend
+import faiss
+import numpy as np
+from scipy.cluster.vq import vq
+import cv2
+from numpy.linalg import norm
 
-# class CBRI:
-#     def __init__(self):
-#         self.detector = SuperPointFrontend(weights_path='./my_utils/superpoint_v1.pth', nms_dist=4, conf_thresh=0.015, nn_thresh=0.7)
-#         self.
-#     def embed(self):
-#       pass
+class CBRI:
+    def __init__(self):
+        self.detector = SuperPointFrontend(weights_path='./my_utils/superpoint_v1.pth', nms_dist=4, conf_thresh=0.015, nn_thresh=0.7)
+        self.kmeans = np.load('./my_utils/centroids.npy')
+        self.idf = np.load('./my_utils/idf.npy')
+        self.tfidf = np.load('./my_utils/tfidf.npy')
+        
+    def embed(self, img):
+      if len(img.shape) == 3:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        
+      kp, desc, heat = self.detector.run(img.astype('float32') / 255.)
+      # self.desc = desc.T
+      img_visual_words, dis = vq(desc.T, self.kmeans)
+      emm = np.zeros(256)
+      for w in img_visual_words:
+        emm[w] += 1
+      
+      return emm * self.idf
+    
+    def top_k(self, que, k):
+      emb = self.embed(que)
+      b = self.tfidf
+      cos = np.dot(emb, b.T) / (norm(emb) * norm(b, axis=1))
+      idx = np.argsort(-cos)[:k]
+      
+    
--- pages/Instance Search.py
+++ pages/Instance Search.py
@@ -1,5 +1,13 @@
 import streamlit as st
 import cv2 as cv
 import matplotlib.pyplot as plt
+from my_utils.cbri import CBRI
 
 st.title('Instance Search')
+a = CBRI()
+print(a.kmeans)
+st.write('1. Dataset')
+
+st.write('2. Methods')
+st.write('3. Evaluation')
+st.write('4. Results')
