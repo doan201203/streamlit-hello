4ba35f6ddb38fc8681ad56b22c02fbddec0a3ee7
doan201203 <doan201203@gmail.com>
2024-11-04 04:33:03 +0700

    DONE: keypoint matching

--- pages/Instance Search.py
+++ pages/Instance Search.py
@@ -7,7 +7,9 @@ st.title('Instance Search')
 a = CBRI()
 print(a.kmeans)
 st.write('1. Dataset')
-
+st.write("""
+            - Trích từ bộ dữ liệu COCO-2017 với đối tượng ***Vehicle*** gồm  
+         """)
 st.write('2. Methods')
 st.write('3. Evaluation')
 st.write('4. Results')
